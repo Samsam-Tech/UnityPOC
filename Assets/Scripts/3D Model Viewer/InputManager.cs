8cc8fe35f80ef61178b459e2d953a408daa60db3
Ayesha Siroy <siroy.ayesha@gmail.com>
2022-11-03 18:16:57 +0800

    fix: model rotate and zoom functionality

--- Assets/Scripts/3D Model Viewer/InputManager.cs
+++ Assets/Scripts/3D Model Viewer/InputManager.cs
@@ -1,28 +1,21 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using TMPro;
 
 public class InputManager : MonoBehaviour
 {
     TouchControls touchControls;
     Coroutine currentCoroutine;
-    Transform cameraTransform;
     Camera cameraMain;
-    [SerializeField] float zoomSpeed = 9f;
-    [SerializeField] float panningSpeed = 0.001f;
-    [SerializeField] GameObject cube;
-    [SerializeField] TextMeshProUGUI distanceText;
-    [SerializeField] TextMeshProUGUI previousDistanceText;
-    [SerializeField] TextMeshProUGUI panningText;
-    [SerializeField] float distanceAllowance = 1.5f;
+    [SerializeField] float zoomSpeed = 1f;
+    [SerializeField] float rotationSpeed = 0.1f;
+    public GameObject model;
     float secondaryTouchContact = 0;
+
 
     private void Awake()
     {
         touchControls = new TouchControls();
-        cameraTransform = Camera.main.transform;
     }
 
     private void OnEnable()
@@ -48,11 +41,11 @@ public class InputManager : MonoBehaviour
 
     private void RotateStart(InputAction.CallbackContext context)
     {
+        Vector3 initialPosition = touchControls.Touch.PrimaryFingerPosition.ReadValue<Vector2>();
 
-
-        if (!touchControls.Touch.SecondaryTouchContact.IsPressed())
+        if (secondaryTouchContact == 0)
         {
-            currentCoroutine = StartCoroutine(Rotation());
+            currentCoroutine = StartCoroutine(Rotation(initialPosition));
         }
 
     }
@@ -62,121 +55,87 @@ public class InputManager : MonoBehaviour
         {
             StopCoroutine(currentCoroutine);
         }
-
     }
 
-    IEnumerator Rotation()
+    IEnumerator Rotation(Vector3 initialPosition)
     {
-        Vector3 initialPosition = touchControls.Touch.PrimaryFingerPosition.ReadValue<Vector2>();
-        float rotationSpeed = 0.2f;
-        Quaternion initialObjRotation = cube.transform.rotation;
+        Vector3 modelInitialPos = initialPosition;
+        Quaternion initialObjRotation = model.transform.rotation;
 
         while (true)
         {
             Vector3 currentPosition = touchControls.Touch.PrimaryFingerPosition.ReadValue<Vector2>();
 
-            if (initialPosition != currentPosition)
+            if (modelInitialPos != currentPosition)
             {
+
                 float distanceBetweenPosX = currentPosition.x - initialPosition.x;
                 float distanceBetweenPosY = currentPosition.y - initialPosition.y;
 
                 float rotateX = rotationSpeed * distanceBetweenPosX;
                 float rotateY = rotationSpeed * distanceBetweenPosY;
-
-                cube.transform.rotation = Quaternion.Euler(rotateY, -rotateX, 0f) * initialObjRotation;
-            }
-
-            if (touchControls.Touch.SecondaryTouchContact.IsPressed())
-            {
-                yield break; //end a Coroutine before it's finished
+                model.transform.rotation = Quaternion.Euler(rotateY, -rotateX, 0f) * initialObjRotation;
             }
 
             yield return null;
 
-
+            if (secondaryTouchContact == 1)
+            {
+                yield break;
+            }
         }
     }
 
     private void ZoomStart(InputAction.CallbackContext context)
     {
+        secondaryTouchContact = context.ReadValue<float>();
         currentCoroutine = StartCoroutine(ZoomDetection());
     }
 
     private void ZoomEnd(InputAction.CallbackContext context)
     {
+        secondaryTouchContact = context.ReadValue<float>();
         if (currentCoroutine != null)
         {
             StopCoroutine(currentCoroutine);
+            secondaryTouchContact = 0;
         }
 
     }
-
-    // IEnumerator PanDetection()
-    // {
-    //     Vector3 initialPrimaryFingerPosition = touchControls.Touch.PrimaryFingerPosition.ReadValue<Vector2>();
-    //     Vector3 initialSecondaryFingerPosition = touchControls.Touch.SecondaryFingerPosition.ReadValue<Vector2>();
-    //     Vector3 currentCamPosition = cameraTransform.position;
-    //     while (true)
-    //     {
-    //         Vector3 currentPrimaryFingerPosition = touchControls.Touch.PrimaryFingerPosition.ReadValue<Vector2>();
-    //         Vector3 currentSecondaryFingerPosition = touchControls.Touch.SecondaryFingerPosition.ReadValue<Vector2>();
-    //     }
-    // }
 
     IEnumerator ZoomDetection()
     {
         float previousDistance = Vector2.Distance(touchControls.Touch.PrimaryFingerPosition.ReadValue<Vector2>(),
             touchControls.Touch.SecondaryFingerPosition.ReadValue<Vector2>());
+        float distance = 0f;
         Vector3 initialPrimaryFingerPosition = touchControls.Touch.PrimaryFingerPosition.ReadValue<Vector2>();
         Vector3 initialSecondaryFingerPosition = touchControls.Touch.SecondaryFingerPosition.ReadValue<Vector2>();
-        Vector3 currentCamPosition = cameraTransform.position;
-        previousDistanceText.SetText("Previous Distance: " + previousDistance);
+        Vector3 currentCamPosition = cameraMain.transform.position;
 
         while (true)
         {
             //Detection
-            float distance = Vector2.Distance(touchControls.Touch.PrimaryFingerPosition.ReadValue<Vector2>(),
+            distance = Vector2.Distance(touchControls.Touch.PrimaryFingerPosition.ReadValue<Vector2>(),
             touchControls.Touch.SecondaryFingerPosition.ReadValue<Vector2>());
             Vector3 currentPrimaryFingerPosition = touchControls.Touch.PrimaryFingerPosition.ReadValue<Vector2>();
             Vector3 currentSecondaryFingerPosition = touchControls.Touch.SecondaryFingerPosition.ReadValue<Vector2>();
 
-            // distanceText.SetText("Distance: " + distance);
-
             // Zoom in
             if (distance > previousDistance)
             {
-                Vector3 targetPosition = cameraTransform.position;
-                targetPosition.z += 1;
-                cameraTransform.position = Vector3.Slerp(cameraTransform.position, targetPosition, Time.deltaTime * zoomSpeed);
-                panningText.SetText("ZOOM IN!");
+                float targetDistance = distance;
+                Vector3 movementAmount = new Vector3(0, 0, targetDistance) * zoomSpeed * Time.deltaTime;
+                cameraMain.transform.Translate(movementAmount);
+                // cameraMain.fieldOfView = Mathf.MoveTowards(cameraMain.fieldOfView, -targetDistance, zoomSpeed * Time.deltaTime); //speed to 30
             }
             // Zoom out
             else if (distance < previousDistance)
             {
-                Vector3 targetPosition = cameraTransform.position;
-                targetPosition.z -= 1;
-                cameraTransform.position = Vector3.Slerp(cameraTransform.position, targetPosition, Time.deltaTime * zoomSpeed);
-                panningText.SetText("ZOOM OUT!");
+                float targetDistance = distance;
+                Vector3 movementAmount = new Vector3(0, 0, -targetDistance) * zoomSpeed * Time.deltaTime;
+                cameraMain.transform.Translate(movementAmount);
+                // cameraMain.fieldOfView = Mathf.MoveTowards(cameraMain.fieldOfView, targetDistance, zoomSpeed * Time.deltaTime); //speed to 30
             }
-            // else if (distance == previousDistance)
-            // {
-            // panningText.SetText("PANNING!");
-            // if (currentPrimaryFingerPosition != initialPrimaryFingerPosition)
-            // {
-            //     float distanceBetweenPosX = currentPrimaryFingerPosition.x - initialPrimaryFingerPosition.x;
-            //     float distanceBetweenPosY = currentPrimaryFingerPosition.y - initialPrimaryFingerPosition.y;
-
-            //     float posX = distanceBetweenPosX * panningSpeed * Time.deltaTime;
-            //     float posY = distanceBetweenPosY * panningSpeed * Time.deltaTime;
-
-            //     // cameraTransform.Translate(-posX, -posY, 0f);
-
-            //     Vector3 newCamPos = new Vector3(-(posX - currentCamPosition.x), -(posY - currentCamPosition.y), currentCamPosition.z);
-
-            //     Camera.main.transform.position = newCamPos;
-
-            // }
-            // }
 
             previousDistance = distance;
             yield return null; //waits until next frame
